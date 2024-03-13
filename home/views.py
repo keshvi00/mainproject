3389c40609fba7f135d450e715e5e628ffb61343
Jary0712 <nirjaris21@gmail.com>
2024-03-13 12:11:34 +0530

    undo

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
-from django.shortcuts import render
+
 from home.models import Admin
 from .forms import tasksentry
 
@@ -32,9 +32,3 @@ def delete_data(request,id):
         pi.delete()
         return HttpResponseRedirect('/')
 
-def dashboard(request):
-    data = {
-        'labels': ['January', 'February', 'March', 'April', 'May'],
-        'values': [10, 20, 15, 25, 30]  # Sample data
-    }
-    return render(request, 'dashboard.html', {'data': data})
